26ff4120ea678c88615412ed2eaef8a7cb9bae19
Jaewoong Hwang <noa9925@gmail.com>
2023-04-09 00:02:43 +0900

    230408
    1. Props with input tag

--- 1. CA/blog/src/App.js
+++ 1. CA/blog/src/App.js
@@ -7,12 +7,16 @@ function App() {
     "우리동네 우동 맛집",
     "소금빵 맛집",
   ]);
-  let [like, setLike] = useState([0, 0, 0]);
-  let [modal, setModal] = useState(true);
 
-  [1, 2, 3].map(function (a) {
-    return "123";
-  });
+  // 글마다 모달창의 제목이 바뀌게 바인딩
+  let [titleIdx, setTitleIdx] = useState(0);
+  let [inputText, setInputText] = useState("");
+
+  let [like, setLike] = useState([0, 0, 0]);
+  let [date, setDate] = useState(["2023/2/28", "2023/1/13", "2023/1/7"]);
+
+  // 모달창 출력을 위한 State 생성 : Boolean 값으로 토글 제어
+  let [modal, setModal] = useState(true);
 
   return (
     <div className="App">
@@ -20,7 +24,7 @@ function App() {
         <h4>wz.archive</h4>
       </div>
 
-      <button
+      {/* <button
         // 글 제목 오름차순 정렬
         className="btnSort"
         onClick={() => {
@@ -30,50 +34,7 @@ function App() {
         }}
       >
         글 정렬
-      </button>
-
-      {/* <div className="list">
-        <h4>
-          {context[0]}{" "}
-          <span
-            // 좋아요 클릭 시 +1
-            onClick={() => {
-              setLike(like + 1);
-            }}
-          >
-            👍{" "}
-          </span>
-          {like[0]}
-        </h4>
-        <p>2023년 4월 5일</p>
-      </div> */}
-
-      {/* <div className="list">
-        <h4
-          onClick={() => {
-            // setContext(["떡볶이 맛집", "우리동네 규동 맛집", "소금빵 맛집"]);
-            // 근데 State 값 하나만 변경하고 싶어도 어레이 전부 가져와야 될까?
-            let copy = [...context];
-            copy[1] = "우리동네 규동맛집";
-            setContext(copy);
-          }}
-        >
-          {context[1]}
-        </h4>
-        <p>2023년 4월 5일</p>
-      </div> */}
-
-      {/* <div className="list">
-        <h4
-          // Boolean 제어를 통한 모달창 토글
-          onClick={() => {
-            setModal(!modal);
-          }}
-        >
-          {context[2]}
-        </h4>
-        <p>2023년 4월 5일</p>
-      </div> */}
+      </button> */}
 
       {/* map함수를 통한 State 바인딩 */}
       {context.map(function (item, i) {
@@ -82,11 +43,13 @@ function App() {
             <h4
               onClick={() => {
                 setModal(!modal);
+                setTitleIdx(i);
               }}
             >
               {context[i]}{" "}
               <span
-                onClick={() => {
+                onClick={(e) => {
+                  e.stopPropagation();
                   let copy = [...like];
                   copy[i] = copy[i] + 1;
                   setLike(copy);
@@ -95,9 +58,28 @@ function App() {
                 {" "}
                 👍 {like[i]}
               </span>
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  let copy = [...context];
+                  copy.splice(i, 1);
+                  setContext(copy);
+
+                  let copyLike = [...like];
+                  copyLike.splice(i, 1);
+                  setLike(copyLike);
+
+                  let copyDate = [...date];
+                  copyDate.splice(i, 1);
+                  setDate(copyDate);
+                }}
+                style={{ marginLeft: "10px" }}
+              >
+                삭제
+              </button>
             </h4>
 
-            <p>2023년 4월 5일</p>
+            <p>{date[i]}</p>
           </div>
         );
       })}
@@ -106,10 +88,53 @@ function App() {
       {modal === false ? (
         <Modal
           setContext={setContext}
-          color={"skyblue"}
+          titleIdx={titleIdx}
           context={context}
         ></Modal>
       ) : null}
+
+      <div style={{ marginTop: "10px" }}>
+        <input
+          onChange={(e) => {
+            setInputText(e.target.value);
+          }}
+          value={inputText}
+        />
+        <button
+          onClick={() => {
+            if (inputText !== "") {
+              // 작성한 Input을 State에 따로 저장해 Context State로 복사
+              let copy = [...context];
+              copy.unshift(inputText);
+              setContext(copy);
+              setInputText("");
+
+              // 좋아요 갯수 또한 갱신, 글과 같이 밀리면서 추가됨
+              let copyLike = [...like];
+              copyLike.unshift(0);
+              setLike(copyLike);
+
+              // 날짜 갱신
+              let copyDate = [...date];
+
+              let today = new Date();
+              let yy = today.getFullYear();
+              let mm = today.getMonth();
+              let dd = today.getDate();
+
+              copyDate.unshift(yy + "/" + mm + "/" + dd);
+              setDate(copyDate);
+
+              // 글작성 성공한 경우 Input 폼을 비우고 싶은데..?
+              // input탭에서 value를 inputText State로 지정해주었고,
+              // 버튼 클릭 시 InputText State를 비워줬음. 성공!
+            }
+          }}
+          style={{ marginLeft: "10px" }}
+        >
+          작성
+        </button>
+      </div>
     </div>
   );
 }
@@ -117,20 +142,11 @@ function App() {
 // 모달 컴포넌트 생성
 function Modal(props) {
   return (
-    <div className="Modal" style={{ background: props.color }}>
-      <h4>{props.context[0]}</h4>
+    <div className="Modal">
+      <h4>{props.context[props.titleIdx]}</h4>
       <p>날짜</p>
       <p>내용</p>
-      <button
-        onClick={() => {
-          // 글수정 버튼 클릭 시 제목 바뀌게, 근데 Array들 엮여서 좀 이상하게 돌아감
-          let copy = [...props.context];
-          copy[0] = "우리동네 치즈맛집";
-          props.setContext(copy);
-        }}
-      >
-        글수정
-      </button>
+      <button>글수정</button>
     </div>
   );
 }
