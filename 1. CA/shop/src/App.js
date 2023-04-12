1afcc568d65378f4063789725196c148e3bf08f1
Jaewoong Hwang <noa9925@gmail.com>
2023-04-12 13:50:15 +0900

    230412
    1. import / export
    2. Product Components

--- 1. CA/shop/src/App.js
+++ 1. CA/shop/src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import data from "./shoesData.js";
+
+// import ingre1 from "./ingre1.png";
 
 import { useState } from "react";
-import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import { Button, Navbar, Container, Nav, Row, Col } from "react-bootstrap";
 
 function App() {
+  const thumbnail = {
+    backgroundColor: "#e5e3e4",
+    marginLeft: "20px",
+  };
+
+  // Export / Import를 통한 오브젝트 데이터 반입
+  let [shoes, setShoes] = useState(data);
+
   return (
     <div className="App">
       <Navbar variant="dark" className="navBar">
@@ -19,7 +30,51 @@ function App() {
           </Nav>
         </Container>
       </Navbar>{" "}
+      <div className="main-bg"></div>
+      <Container style={{ marginTop: "20px" }}>
+        <Row>
+          {/* <Col sm>
+            <img
+              src={process.env.PUBLIC_URL + "/ingre3.png"}
+              className="Thumb"
+            />
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].content}</p>
+            <p>{shoes[0].price}</p>
+          </Col>
+          <Col sm ml3>
+            <img
+              src={process.env.PUBLIC_URL + "/ingre2.png"}
+              className="Thumb"
+            />
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].content}</p>
+            <p>{shoes[1].price}</p>
+          </Col>
+          <img src={process.env.PUBLIC_URL + "/ingre4.png"} className="Thumb" />
+          <h4>{shoes[2].title}</h4>
+          <p>{shoes[2].content}</p>
+          <p>{shoes[2].price}</p> */}
+
+          {shoes.map(function (item, i) {
+            return <Thumbnail shoes={shoes} idx={i}></Thumbnail>;
+          })}
+        </Row>
+      </Container>
     </div>
+  );
+}
+
+function Thumbnail(props) {
+  return (
+    <Col sm>
+      <div className="Thumbnail">
+        <img src={props.shoes[props.idx].imgPath} className="Thumb" />
+        <h4>{props.shoes[props.idx].title}</h4>
+        <p>{props.shoes[props.idx].content}</p>
+        <p>{props.shoes[props.idx].price}</p>
+      </div>
+    </Col>
   );
 }
 
---
+++ 1. CA/shop/src/shoesData.js
@@ -0,0 +1,27 @@
+let data = [
+  {
+    id: 0,
+    title: "Meat",
+    content: "Very Fresh",
+    price: "12,000₩ / kg",
+    imgPath: process.env.PUBLIC_URL + "/ingre3.png",
+  },
+
+  {
+    id: 1,
+    title: "Carrot",
+    content: "Very Hard",
+    price: "1,100₩ / per",
+    imgPath: process.env.PUBLIC_URL + "/ingre2.png",
+  },
+
+  {
+    id: 2,
+    title: "Lettuce",
+    content: "Very Greenery",
+    price: "2,300₩ / per",
+    imgPath: process.env.PUBLIC_URL + "/ingre4.png",
+  },
+];
+
+export default data;
