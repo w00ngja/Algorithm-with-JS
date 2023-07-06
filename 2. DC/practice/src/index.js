f5c0380a8907f57784bd3d0e697458f908330f8c
Jaewoong Hwang <noa9925@gmail.com>
2023-07-06 16:29:12 +0900

    20230706

--- 2. DC/practice/src/index.js
+++ 2. DC/practice/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AppMentor from "./AppMentor";
 import reportWebVitals from "./reportWebVitals";
+import AppTheme from "./AppTheme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppMentor />
+    <AppTheme />
   </React.StrictMode>
 );
 
