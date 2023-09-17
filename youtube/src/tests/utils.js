98ca06dc9ee23448e668388af4d12d67babca149
Jaewoong Hwang <noa9925@gmail.com>
2023-09-17 18:34:03 +0900

    20230917 (2)

--- youtube/src/tests/utils.js
+++
@@ -1,9 +0,0 @@
-import { MemoryRouter, Routes } from 'react-router-dom';
-
-export function withRouter(routes, initialEntry = '/') {
-  return (
-    <MemoryRouter initialEntries={[initialEntry]}>
-      <Routes>{routes}</Routes>
-    </MemoryRouter>
-  );
-}
